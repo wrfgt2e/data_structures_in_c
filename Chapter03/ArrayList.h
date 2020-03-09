6f041c2fc559aed94a619ff81d3a115677a1946f
gipark <gipark@student.42seoul.kr>
2020-03-10 00:10:25 +0900

    Linked List 1

---
+++ Chapter03/ArrayList.h
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ArrayList.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gipark <gipark@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/09 20:58:56 by gipark            #+#    #+#             */
+/*   Updated: 2020/03/09 21:10:47 by gipark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_LIST_H
+# define ARRAY_LIST_H
+
+#define TRUE        1       // '참'을 표현하기 위한 매크로 정의
+#define FALSE       0       // '거짓'을 표현하기 위한 매크로 정의
+
+#define LIST_LEN    100
+typedef int LData;          // LData에 대한 typedef 선언
+
+typedef struct __ArrayList      // 배열기반 리스트를 정의한 구조체
+{
+    LData arr[LIST_LEN];        // 리스트의 저장소인 배열
+    int numOfData;              // 저장된 데이터의 수 
+    int curPosition;            // 데이터 참조위치를 기록
+} ArrayList;
+
+typedef ArrayList List;
+
+void    ListInit(List *plist);                  // 초기화
+void    LInsert(List *plist, LData pdata);      // 데이터 저장
+
+int     LFirst(List *plist, LData *pdata);      // 첫 데이터 참조
+int     LNext(List *plist, LData *pdata);       // 두 번째 이후 데이터 참조
+
+LData   LRemove(List *plist);                   // 참조한 데이터 삭제
+int     LCount(List *plist);                    // 저장된 데이터의 수 반환
+
+#endif
---
+++ Chapter03/ListUseMain.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ListUseMain.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gipark <gipark@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/09 21:37:45 by gipark            #+#    #+#             */
+/*   Updated: 2020/03/09 22:15:14 by gipark           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "ArrayList.h"
+
+int main(void)
+{
+    // ArrayList의 생성 및 초기화 ////////
+    List    list;
+    int     data;
+    int     i;
+    int     sum;
+    ListInit(&list);
+
+    // 정수 1부터 9까지 저장 ////////
+    i = 1;
+    while (i < 10)
+        LInsert(&list, i++);
+    
+    // 
+    sum = 0;
+    if (LFirst(&list, &data))
+    {
+        sum += data;
+        while (LNext(&list, &data))
+            sum += data;
+    }
+    printf("리스트에 저장된 값의 합: %d \n", sum);
+
+    if (LFirst(&list, &data))
+    {
+        if ((data % 2 == 0) || (data % 3 == 0))
+            LRemove(&list);
+        while (LNext(&list, &data))
+        {
+            if ((data % 2 == 0) || (data % 3 == 0))
+                LRemove(&list);
+        }
+    }
+    
+    if (LFirst(&list, &data))
+    {
+        printf("%d ", data);
+        while (LNext(&list, &data))
+            printf("%d ", data);
+    }
+    printf("\n\n");
+    return (0);
+}
